ca278fc9c329c3bf009cff3e546bd4607101ef7f
SakraKing <143229922+SakraKing@users.noreply.github.com>
2024-03-25 19:06:09 -0300

    Add files via upload

---
+++ Modificar valor con funcion.cpp.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int ModificarValor(int &a);
+
+int main (){
+
+int a;
+
+a=1;
+
+ModificarValor(a);
+
+cout << a;
+return 0;
+
+}
+
+int ModificarValor(int &a){
+
+a=a*2;
+
+}
